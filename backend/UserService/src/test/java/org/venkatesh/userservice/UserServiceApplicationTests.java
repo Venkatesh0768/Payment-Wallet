fca5dfb164a0084a95b4676540a43b468f2efd12
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2025-11-25 17:24:24 +0530

    Setup the db and eureka server on the docker

---
+++ backend/UserService/src/test/java/org/venkatesh/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package org.venkatesh.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootTest
+@EnableDiscoveryClient
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
